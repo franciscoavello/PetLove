56b9719661bc119499478a304c3607c212ecc1c1
Francisco Avello Barrera <francisco.avello@usach.cl>
2016-11-10 13:57:23 -0300

    Iconos de la app mejorados

--- app/src/main/java/com/usach/uxyappsmoviles/petloveprueba/InformacionMascotaActivity.java
+++ app/src/main/java/com/usach/uxyappsmoviles/petloveprueba/InformacionMascotaActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
